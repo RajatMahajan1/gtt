55ae3ce526297e59997d095b304f462551e6ce0f
rajat <rajatmahajan307@gmail.com>
2019-09-17 14:40:59 +0530

    v2

---
+++ Dollar.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Dollar
+{
+public static void main(String args)
+{
+Scanner sc=new Scanner(System.in);
+int n,m;
+int k=0;
+int c=0;
+n=sc.nextInt();
+int arr[]=new int[n];
+for(int i=0;i<n;i++)
+{
+arr[i]=sc.nextInt();
+}
+m=sc.nextInt();
+int arr[]=new int[m];
+for(j=0;j<m;j++)
+{
+arr[i]=sc.nextInt();
+}
+	Arrays.sort(arr);
+Arrays.sort(ar);
+for(int i=0;i<n;i++)
+if (ar[ar.length-1]>arr[i])
+{
+whiel(arr[i]<=ar[ar.length-1])
+{
+arr[i]=arr[i]+1;
+c++
+}
+}
+}
+System.out.println(c);
+}
+}
